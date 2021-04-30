53d64651ef1227aa226b0e639f4dadf1ca4fbe6c
Daniel Pardo Pont <daparpon@gmail.com>
2021-04-30 12:55:20 +0200

    Publicado endpoint de base de datos remota

--- src/main/java/es/conselleria/daparpon/techmarket/utils/DBParams.java
+++ src/main/java/es/conselleria/daparpon/techmarket/utils/DBParams.java
@@ -6,16 +6,8 @@ package es.conselleria.daparpon.techmarket.utils;
  */
 public class DBParams {
     /* Remote database connection parameters */
-    /*
     public static final String DB_URL = "jdbc:mysql://techmarketawsdb.csdv5n6tajtd.eu-west-3.rds.amazonaws.com:3306";
     public static final String DB_NAME = "techmarket";
     public static final String DB_USER = "rootAdminAWS";
     public static final String DB_PASSWORD = "$vOWOll#8hCrJ3nd";
-    */
-    
-    /* Local database connection parameters */
-    public static final String DB_URL = "jdbc:mysql://localhost:3306";
-    public static final String DB_NAME = "techmarket";
-    public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "Y7H6yRMj3mudEZmC";
 }
